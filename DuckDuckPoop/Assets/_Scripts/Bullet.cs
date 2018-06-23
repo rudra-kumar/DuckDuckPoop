e7f467c4d114c3421d234022c836f5d596735be4
Amshumaan Rakshith <amshumaanrakshith@gmail.com>
2018-06-23 15:10:09 +1000

    Added Unity Project
    Project containing the player prefab with working planet movement and bullet firing mechanics.

---
+++ DuckDuckPoop/Assets/_Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    [SerializeField] float bullSpeed = 100.0f;
+    Vector3 planet;
+    Vector3 forward;
+
+	void Start () {
+        // Getting the planets position
+        planet = GameObject.FindGameObjectWithTag("Planet").transform.position;
+        forward = transform.forward;
+	}
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(planet, forward, bullSpeed * Time.deltaTime);
+    }
+
+}
---
+++ DuckDuckPoop/Assets/_Scripts/FauxGravityBody.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour {
+
+    [SerializeField] FauxGravityAttractor attractor;
+    private Transform myTransform;
+
+    void Start()
+    {
+        // Get Attractor component from Planet
+        if (attractor == null) attractor = GameObject.FindWithTag("Planet").GetComponent<FauxGravityAttractor>();
+
+        // Get rigid body component reference
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation; // Disable Rotation
+        rb.useGravity = false;                                // Disable Gravity
+        
+        // Get transform reference
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(myTransform);
+    }
+}
